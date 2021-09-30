b7758b99ee6710874ba45b120247a4120992d6b6
sayog4 <sayogkarki4@gmail.com>
2021-09-30 21:02:16 +0545

    fix axios url and add types packages to dependencies

--- client/src/axios/axiosInstance.ts
+++ client/src/axios/axiosInstance.ts
@@ -1,5 +1,8 @@
 import axios, { AxiosRequestConfig } from 'axios'
-const API_URL = `http://localhost:5000`
+const API_URL =
+  process.env.NODE_ENV === 'development'
+    ? `http://localhost:5000`
+    : `https://findjobs1.herokuapp.com`
 const config: AxiosRequestConfig = {
   baseURL: API_URL,
   withCredentials: true,
